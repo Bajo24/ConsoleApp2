b7bb2b8173ff32452738597969b574886cc3fe6b
Joaquin <Joaquin@LAPTOP-GCOL87CE>
2026-09-14 23:02:27 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int N;
+            int cantidadCeros = 0;
+            int cantidadVeintes = 0;
+            int aprobados = 0;
+            int desaprobados = 0;
+
+            Console.Write("Ingrese el número de alumnos (máximo 10): ");
+            N = int.Parse(Console.ReadLine());
+
+            double[] notas = new double[N];
+
+            // Ingresar las notas
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Ingrese la nota del alumno " + (i + 1) + ": ");
+                notas[i] = double.Parse(Console.ReadLine());
+            }
+
+            // Analizar las notas
+            for (int i = 0; i < N; i++)
+            {
+                if (notas[i] == 0)
+                {
+                    cantidadCeros++;
+                }
+
+                if (notas[i] == 20)
+                {
+                    cantidadVeintes++;
+                }
+
+                if (notas[i] >= 13)
+                {
+                    aprobados++;
+                }
+                else
+                {
+                    desaprobados++;
+                }
+            }
+
+            // Mostrar resultados
+            Console.WriteLine("\n--- RESULTADOS ---");
+            Console.WriteLine("Cantidad de personas que obtuvieron 00: " + cantidadCeros);
+            Console.WriteLine("Cantidad de personas que obtuvieron 20: " + cantidadVeintes);
+            Console.WriteLine("Cantidad de personas aprobadas: " + aprobados);
+            Console.WriteLine("Cantidad de personas desaprobadas: " + desaprobados);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+  
+            static void Main()
+            {
+                int N;
+                double suma = 0;
+                double promedio;
+                double maxima;
+                double minima;
+
+                Console.Write("Ingrese el número de alumnos: ");
+                N = int.Parse(Console.ReadLine());
+
+                double[] notas = new double[N];
+
+                // Ingresar las notas
+                for (int i = 0; i < N; i++)
+                {
+                    Console.Write("Ingrese la nota del alumno " + (i + 1) + ": ");
+                    notas[i] = double.Parse(Console.ReadLine());
+
+                    suma += notas[i];
+                }
+
+                // Inicializar máxima y mínima
+                maxima = notas[0];
+                minima = notas[0];
+
+                // Buscar máxima y mínima
+                for (int i = 1; i < N; i++)
+                {
+                    if (notas[i] > maxima)
+                    {
+                        maxima = notas[i];
+                    }
+
+                    if (notas[i] < minima)
+                    {
+                        minima = notas[i];
+                    }
+                }
+
+                // Calcular promedio
+                promedio = suma / N;
+
+                // Mostrar resultados
+                Console.WriteLine("\n--- RESULTADOS ---");
+                Console.WriteLine("Promedio de notas: " + promedio);
+                Console.WriteLine("Nota máxima: " + maxima);
+                Console.WriteLine("Nota mínima: " + minima);
+            }
+        
+    }   
+}
